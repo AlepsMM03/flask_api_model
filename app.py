05599bbc0deefa0249db1858453af01e93ca9acf
Alejandro Montes <95830903+AlepsMM03@users.noreply.github.com>
2025-05-23 16:43:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ CORS(app)
 app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
