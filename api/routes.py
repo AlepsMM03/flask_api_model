fb42cb0c3e730c1279f9c495ccda618a53853e58
Alejandro Montes <95830903+AlepsMM03@users.noreply.github.com>
2025-05-23 16:32:04 -0600

    Update routes.py

--- api/routes.py
+++ api/routes.py
@@ -7,16 +7,18 @@ api_bp = Blueprint('api', __name__)
 model_path = os.path.join('model', 'random_forest_model2.pkl')
 model = joblib.load(model_path)
 
+@api_bp.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "✅ API de predicción de infarto activa"})
+
 @api_bp.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
 
-    # Verifica que los campos esperados estén presentes
     expected_fields = ['Troponin', 'CK-MB', 'Age']
     if not all(field in data for field in expected_fields):
         return jsonify({"error": f"Faltan uno o más campos requeridos: {expected_fields}"}), 400
 
-    # Extrae las características en el orden correcto
     features = np.array([
         data['Troponin'],
         data['CK-MB'],
@@ -25,7 +27,6 @@ def predict():
 
     prediction = model.predict(features)[0]
 
-    # Traducción del resultado
     diagnosis = "Positivo para infarto" if prediction == 1 else "Negativo para infarto"
 
     return jsonify({
